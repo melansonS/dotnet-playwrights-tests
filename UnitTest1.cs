5bb79e9c0446ed5fb15ef907fad8a408e8446898
Samuel Melanson <samuelmelanson@live.com>
2023-07-12 13:42:35 -0400

    first unit test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,15 +1,27 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
 namespace PlaywrightTests;
 
-public class Tests
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public async Task HomepageHasmelansonsInTitleAndHiImSamuelMelansonHeading()
     {
-        Assert.Pass();
+        await Page.GotoAsync("https://melansons.vercel.app/");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync("melanson[s]");
+
+        // create a locator
+        var headingElement = Page.GetByRole(AriaRole.Heading, new PageGetByRoleOptions() { Name = "Hi, I'm samuel Melanson" });
+
+        // Expect an attribute "to be strictly equal" to the value.
+        await Expect(headingElement).ToHaveTextAsync(new Regex("Hi, I'm Samuel Melanson"));
     }
 }
